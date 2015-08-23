bde8677441e498843b112570d2ed0b68b87f7f5b
george <ruan3@illinois.edu>
2015-08-23 00:01:07 -0500

    Remove unused code.

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -10,9 +10,9 @@
   angular.module('georgeRuan')
     .controller('HomeCtrl', homeCtrl);
 
-    homeCtrl.$inject = ['$scope', '$location', '$anchorScroll', 'UserData'];
+    homeCtrl.$inject = ['$scope', 'UserData'];
 
-    function homeCtrl ($scope, $location, $anchorScroll, UserData) {
+    function homeCtrl ($scope, UserData) {
       UserData.getName().then(setName);
 
       /**
@@ -21,19 +21,6 @@
        */
       function setName (name) {
         $scope.name = name;
-      }
-
-      $scope.scrollToId = scrollToId;
-
-      /**
-       * Scrolls to specified section of the page
-       * @param  {[type]} id The id of the section to scroll to.
-       * @return {String}    Section id
-       */
-      function scrollToId(id) {
-        $location.hash(id);
-        $anchorScroll();
-        return id;
       }
     }
 })();
